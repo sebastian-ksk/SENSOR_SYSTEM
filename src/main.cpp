a67f5bf05abde3581e7134aa92f8d6991e07f79a
Sebastian Castellanos <57274306+sebastian-ksk@users.noreply.github.com>
2020-12-07 10:46:04 -0500

    initial program of sensors

---
+++ src/main.cpp
@@ -0,0 +1,95 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "ClosedCube_HDC1080.h"
+#include <Adafruit_MLX90614.h>
+#include <XBee.h>
+#include <SoftwareSerial.h>
+
+ClosedCube_HDC1080 hdc1080;
+Adafruit_MLX90614 termometroIR = Adafruit_MLX90614();
+XBee xbee = XBee();
+
+uint8_t text[100] = {};
+XBeeAddress64 addr64 = XBeeAddress64(0x000000000, 0x000000000);
+ZBTxRequest zbTx = ZBTxRequest(addr64, text, sizeof(text));
+ZBTxStatusResponse txStatus = ZBTxStatusResponse();
+
+float Temp_dosel=0;
+float Temp_amb=0;
+float HumR=0;
+int ARead_A0=0; 
+int ARead_A1=0; 
+int ARead_A2=0; 
+int ARead_A3=0;
+float V_s=4.47; 
+double AnVol0=0;
+double AnVol1=0;
+double AnVol2=0;
+double AnVol3=0;
+// funciones 
+void printSerialNumber();
+void envia(String D);
+
+void setup() {
+  Serial.begin(9600);
+  xbee.setSerial(Serial);
+  termometroIR.begin();
+  hdc1080.begin(0x40);
+  Serial.println("ClosedCube HDC1080 Arduino Test");
+  Serial.print("Manufacturer ID=0x");
+  Serial.println(hdc1080.readManufacturerId(), HEX); 
+  Serial.print("Device ID=0x");
+  Serial.println(hdc1080.readDeviceId(), HEX);
+  pinMode(LED_BUILTIN, OUTPUT);
+  printSerialNumber();
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(5000);
+  Temp_amb=hdc1080.readTemperature();
+  HumR=hdc1080.readHumidity();
+  Temp_dosel = termometroIR.readObjectTempC();
+  Serial.print("T dosel =");
+  Serial.println(Temp_dosel);
+  Serial.print("T=");
+  Serial.print(Temp_amb);  
+  Serial.print("C, RH=");
+  Serial.print(HumR);
+  Serial.println("% ");
+
+  ARead_A0=analogRead(A0); 
+  ARead_A2=analogRead(A2);  
+  ARead_A1=analogRead(A1);  
+  ARead_A3=analogRead(A3);
+
+  AnVol0 = (ARead_A0*V_s) / 1023;
+  AnVol1 = (ARead_A1*V_s) / 1023;
+  AnVol2 = (ARead_A2*V_s) / 1023;
+  AnVol3 = (ARead_A3*V_s) / 1023;
+
+  Serial.print("Analog sens: ");Serial.println(String(AnVol0)+","+String(AnVol1)+","+String(AnVol2)+","+String(AnVol3)); 
+  String dataStringXbee = String(ARead_A0)+","+String(ARead_A1)+","+String(ARead_A2)+","+String(ARead_A3)+","+ String(HumR)+","+ String(Temp_dosel)+","+String(Temp_amb)+","+"End"+"\n"; 
+  envia(dataStringXbee);
+  Serial.println("------------------------------------------------");
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(5000);
+}
+
+void printSerialNumber() {
+Serial.print("Device Serial Number=");
+HDC1080_SerialNumber sernum = hdc1080.readSerialNumber();
+char format[12];
+sprintf(format, "%02X-%04X-%04X", sernum.serialFirst, sernum.serialMid, sernum.serialLast);
+Serial.println(format);
+}
+
+void envia(String D)
+{
+  for(int i=0;i<=100;i++)
+  {
+    text[i]=D[i];
+  }
+ xbee.send(zbTx);
+}
